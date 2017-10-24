1df28dc67f3b045bf3654d4be55a991ca309d2c5
Rogier <Alex_Rogier@Indy.GoldenRule.Com>
2017-10-24 13:08:05 -0400

    Add project files.

---
+++ MundaneWebAPI/Controllers/MundaneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MundaneWebAPI.Models;
+
+namespace MundaneWebAPI.Controllers
+{
+    public class MundaneController : ApiController
+    {
+        mundanedbEntities ctx = new mundanedbEntities();
+        
+        [HttpGet]
+        public IEnumerable<GetAllItems_Result> GetAllItems(int numResults, bool bEnableMagicalItems)
+        {
+            return ctx.GetAllItems(numResults, bEnableMagicalItems).ToList();
+        }
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
